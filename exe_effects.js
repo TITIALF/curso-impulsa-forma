e2f828304bc8c0a54837b449d435caaddfc545de
TITIALF <titialf@gmail.com>
2025-09-24 21:11:10 -0500

    Update exe_effects.js

--- exe_effects.js
+++ exe_effects.js
@@ -1,165 +1,154 @@
-// Effects Plugin for eXeLearning (REVISADO COMPLETO)
+// Effects Plugin for eXeLearning (CORREGIDO COMPLETO)
 // By Ignacio Gros (http://www.gros.es/) for eXeLearning (http://exelearning.net/)
-// CORRECCIONES por Alfonso Sánchez para restaurar los desplegables
+// CORRECCIONES por Alfonso Sánchez para restaurar desplegables y normalizar HTML
 // Creative Commons Attribution-ShareAlike (http://creativecommons.org/licenses/by-sa/3.0/)
 
 $exeFX = {
-    baseClass : "exe",
-    h2 : "h2",
-    isOldBrowser : false,
+    baseClass: "exe",
+    h2: "h2",
+    isOldBrowser: false,
 
-    // ===== Inicialización principal =====
-    init : function(){
+    init: function () {
         var ie = $exeFX.checkIE();
-        if ((!isNaN(parseFloat(ie)) && isFinite(ie)) && ie<9) {
+        if ((!isNaN(parseFloat(ie)) && isFinite(ie)) && ie < 9) {
             $exeFX.isOldBrowser = true;
             $exeFX.h2 = "H2";
         }
         var k = $exeFX.baseClass;
-        $("."+k+"-fx").each(function(i){
+        var f = $("." + k + "-fx");
+        var hasTimeLines = false;
+        $("." + k + "-fx").each(function (i) {
             var c = this.className;
-            if (c.indexOf(" "+k+"-accordion")!=-1) $exeFX.accordion.init(this,i);
-            else if (c.indexOf(" "+k+"-tabs")!=-1) $exeFX.tabs.init(this,i);
-            else if (c.indexOf(" "+k+"-paginated")!=-1) $exeFX.paginated.init(this,i);
-            else if (c.indexOf(" "+k+"-carousel")!=-1) $exeFX.carousel.init(this,i);
-            else if (c.indexOf(" "+k+"-timeline")!=-1 && document.body.className.indexOf("exe-epub3")!=0) {
-                $exeFX.timeline.init(this,i);
+            if (c.indexOf(" " + k + "-accordion") != -1) $exeFX.accordion.init(this, i);
+            else if (c.indexOf(" " + k + "-tabs") != -1) $exeFX.tabs.init(this, i);
+            else if (c.indexOf(" " + k + "-paginated") != -1) $exeFX.paginated.init(this, i);
+            else if (c.indexOf(" " + k + "-carousel") != -1) $exeFX.carousel.init(this, i);
+            else if (c.indexOf(" " + k + "-timeline") != -1 && document.body.className.indexOf("exe-epub3") != 0) {
+                $exeFX.timeline.init(this, i);
+                hasTimeLines = true;
             }
         });
+        if (hasTimeLines) {
+            setTimeout(function () { $.timeliner({}); }, 500);
+        }
     },
 
-    // ===== Utilidades =====
-    hex2rgb : function(h,a){
-        var hex = parseInt(h.substring(1), 16);
-        var r = (hex & 0xff0000) >> 16;
-        var g = (hex & 0x00ff00) >> 8;
-        var b = hex & 0x0000ff;
-        var c = "rgb";
-        if (a) c += "a";
-        c += "("+r+","+g+","+b+"";
-        if (a) c+= ","+a;
-        c += ")";
-        return c;
-    },
-
-    iframesCheck : function(block){
-        var iframes = $("iframe",block);
-        iframes.each(function(){
-            if (this.src && typeof(this.src)=="string"){
-                var e = $(this);
-                var c = "exeFXcheckedIframe";
-                if (!e.hasClass(c)){
-                    e.addClass(c);
-                    this.src = this.src; // reload once
-                }
-            }
-        });
-    },
-
-    checkIE : function(){
+    checkIE: function () {
         var n = navigator.userAgent.toLowerCase();
         return (n.indexOf('msie') != -1) ? parseInt(n.split('msie')[1]) : false;
     },
 
-    noFX : function(e) {
-        e.attr("class","").css("padding","1em");
+    noFX: function (e) {
+        e.attr("class", "").css("padding", "1em");
     },
 
-    removeXMLNS : function(t) {
-        if (document.body.className.indexOf("exe-epub3")==0) {
-            var xmlnsString = 'h2 xmlns="http://www.w3.org/1999/xhtml"';
-            var xmlnsStringExp = new RegExp(xmlnsString, 'g');
-            t = t.replace(xmlnsStringExp, 'h2');
-        }
-        return t;
-    },
-
-    rftTitles : function(t) {
-        var div = $("<div></div>");
-        div.html(t);
-        $("h2",div).each(function() {
-            var attributes = $.map(this.attributes, function(item) { return item.name; });
-            var title = $(this);
-            $.each(attributes, function(i, item) {
-                if (item.toLowerCase()!="title") title.removeAttr(item);
-            });
+    iframesCheck: function (block) {
+        $("iframe", block).each(function () {
+            if (this.src && typeof (this.src) == "string") {
+                var e = $(this);
+                var c = "exeFXcheckedIframe";
+                if (!e.hasClass(c)) {
+                    e.addClass(c);
+                    this.src = this.src;
+                }
+            }
         });
-        t = div.html();
-        t = $exeFX.removeXMLNS(t);
-
-        var s = t.split('<'+$exeFX.h2+' title="');
-        var n ="";
-        if (s.length<2) return t;
-        for (var i=0;i<s.length;i++) {
-          n += s[i];
-          if (i<(s.length-1)) n += '<'+$exeFX.h2+'><span title="';
-          n = n.replace("</"+$exeFX.h2+">","</span></"+$exeFX.h2+">");
-        }
-        return n;
     },
 
     // ===== Accordion =====
-    accordion : {
-        enable : function(e){
-            $(".fx-accordion-title",e).click(function(ev){
-                ev.preventDefault();
-                var parent = $(this).parent();
-                if($(this).hasClass('active')){
-                    $(this).removeClass('active');
-                    $(this).next('.fx-accordion-content').slideUp(300).removeClass('open');
-                    $(this).find(".fx-arrow").removeClass("rotated");
-                } else {
-                    // Solo se cierra el mismo bloque
-                    $('.fx-accordion-title',parent).removeClass('active');
-                    $('.fx-accordion-content',parent).slideUp(300).removeClass('open');
-                    $('.fx-arrow',parent).removeClass("rotated");
+    accordion: {
+        closeAll: function (parent) {
+            $('.fx-accordion-title', parent).removeClass('active');
+            $('.fx-accordion-content', parent).slideUp(0).removeClass('open');
+            $('.fx-arrow', parent).removeClass('rotated');
+        },
 
-                    $(this).addClass('active');
-                    $(this).next('.fx-accordion-content').slideDown(300).addClass('open');
-                    $(this).find(".fx-arrow").addClass("rotated");
-                }
+        toggle: function (title) {
+            var parent = title.closest('.fx-accordion-section');
+            var content = title.next('.fx-accordion-content');
+            var arrow = title.find('.fx-arrow');
+
+            if (content.hasClass('open')) {
+                content.slideUp(300).removeClass('open');
+                title.removeClass('active');
+                arrow.removeClass('rotated');
+            } else {
+                $exeFX.accordion.closeAll(title.closest('.exe-fx'));
+                content.slideDown(300).addClass('open');
+                title.addClass('active');
+                arrow.addClass('rotated');
+            }
+        },
+
+        enable: function (e) {
+            $(".fx-accordion-title", e).click(function (ev) {
+                ev.preventDefault();
+                $exeFX.accordion.toggle($(this));
             });
         },
-        rft : function(e,i){
-            var html = e.html();
-            html = $exeFX.rftTitles(html);
 
-            // Reemplazo h2 por títulos de acordeón
-            html = html.replace(/<h2>/g, '<h2 class="fx-accordion-title"><span class="fx-arrow">&#9656;</span> ');
-            html = html.replace(/<\/h2>/g, '</h2><div class="fx-accordion-content" style="display:none"></div>');
+        rft: function (e, i) {
+            var html = "";
+            var h = e.html();
+            h = $exeFX.rftTitles(h);
 
-            e.html('<div id="exe-accordion-'+i+'">'+html+'</div>');
+            var p = h.split('<' + $exeFX.h2 + '>');
+            if (p.length == h.split('</' + $exeFX.h2 + '>').length) {
+                for (var x = 1; x < p.length; x++) {
+                    html += '<' + $exeFX.h2 + '>' + p[x];
+                }
+            }
 
-            // Inicializar acordeón
+            if ($exeFX.isOldBrowser) {
+                html = html.replace(/<H2>/g, '<h2 class="fx-accordion-title">');
+            } else {
+                html = html.replace(/<h2>/g, '<h2 class="fx-accordion-title">');
+            }
+
+            html = html.replace(/<\/h2>/g, ' <span class="fx-arrow">&#9662;</span></h2><div class="fx-accordion-content"></div>');
+
+            if (html == "") { $exeFX.noFX(e); return; }
+
+            e.html('<div id="' + $exeFX.baseClass + '-accordion-' + i + '" class="exe-fx">\n<div class="fx-accordion-section">\n' + html + '\n</div>\n</div>\n');
+
+            var sections = $(".fx-accordion-section", e);
+            sections.each(function (index) {
+                var titles = $(".fx-accordion-title", this);
+                titles.each(function (y) {
+                    var content = $(this).next(".fx-accordion-content");
+                    var originalContent = $(this).nextAll().not(".fx-accordion-title").detach();
+                    content.append(originalContent);
+                });
+            });
+
             $exeFX.accordion.enable(e);
         },
-        init : function(x,i){
+
+        init: function (x, i) {
             var e = $(x);
-            var a = $("h2",e);
-            if (a.length>0) $exeFX.accordion.rft(e,i);
+            var a = $("h2", e);
+            if (a.length > 0) $exeFX.accordion.rft(e, i);
             else $exeFX.noFX(e);
         }
     },
 
-    // ===== Tabs =====
-    tabs : {
-        show : function(gID,id){
-            var g = $("#"+gID);
-            $(".fx-tabs li",g).removeClass("fx-current").removeClass("fx-C2");
-            $("#"+id+"-link").addClass("fx-current fx-C2");
-            $(".fx-tab-content",g).removeClass("fx-current");
-            var block = $("#"+id);
-            block.addClass("fx-current");
-            $exeFX.iframesCheck(block);
-        },
-        init : function(e,i){ /* mantener como estaba */ }
-    },
-
-    // ===== Placeholder para otros efectos =====
-    paginated : { init:function(){}, carousel:{init:function(){}} , timeline:{init:function(){}} 
+    rftTitles: function (t) {
+        var div = $("<div></div>");
+        div.html(t);
+        $("h2", div).each(function () {
+            var attributes = $.map(this.attributes, function (item) {
+                return item.name;
+            });
+            var title = $(this);
+            $.each(attributes, function (i, item) {
+                if (item.toLowerCase() != "title") title.removeAttr(item);
+            });
+        });
+        t = div.html();
+        return t;
+    }
 };
 
-// Inicializar al cargar
-$(document).ready(function(){
+$(document).ready(function () {
     $exeFX.init();
 });
